7445861a960d99e7b5214aeea2bc3c5b0c5e3b51
Matteo Horvath <horvathandrasmate@gmail.com>
2024-10-29 09:31:26 +0100

    try it

--- src/ui/matches/actionbar/match-extract-button.tsx
+++ src/ui/matches/actionbar/match-extract-button.tsx
@@ -1,12 +1,25 @@
 import { Button } from 'csdm/ui/components/buttons/button';
 import React from 'react';
 import { useSelectedMatches } from '../use-selected-matches';
+import { buildPlayerFullRoundsSequences } from 'csdm/ui/match/video/sequences/build-player-full-rounds-sequences';
+import type { Match } from 'csdm/common/types/match';
+import { fetchMatchesByChecksums } from 'csdm/node/database/matches/fetch-matches-by-checksums';
+import { useMatchesTable } from '../table/use-matches-table';
 
 export function MatchExtractButton() {
-  const onClick = () => {
+  const selectedMatches = useSelectedMatches();
+
+  const onClick = async () => {
+    const matches: Match[] = await fetchMatchesByChecksums(selectedMatches.map((match) => match.checksum));
+    for (const match of matches) {
+      for (const player of match.players) {
+        const sequences = buildPlayerFullRoundsSequences(match, player.steamId);
+        console.log(sequences);
+        //    await addWholeDemToSeqs(match, sequences);
+      }
+    }
     return 0;
   };
-  const selectedMatches = useSelectedMatches();
 
   if (selectedMatches.length === 0) {
     return null;
